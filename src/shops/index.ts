328fd5ad282e1e3c37d790dae27fcbbdae9f009d
Vladimir Danilin <volodymyr.danilin@gmail.com>
2022-04-19 20:33:44 +0300

    Add more shops

--- src/shops/index.ts
+++ src/shops/index.ts
@@ -1,9 +1,15 @@
 import shop1650117279427 from './1650117279427.json';
 import shop1650211155930 from './1650211155930.json';
 import shop1650213248321 from './1650213248321.json';
+import shop1650386686355 from './1650386686355.json';
+import shop1650387565320 from './1650387565320.json';
+import shop1650389141818 from './1650389141818.json';
 
 export default [
   shop1650117279427,
   shop1650211155930,
   shop1650213248321,
+  shop1650386686355,
+  shop1650387565320, // need to correct price map
+  shop1650389141818, // 2022-04-15 228, need to correct working hours
 ];
